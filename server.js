9a8f760681563ef07afde03a6a06f0d5abe99b14
MVENKATESH0786 <mvenkatesh0786@gmail.com>
2024-11-22 21:46:12 +0530

    "Added empty line in server.js"

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use("/api", addSchoolRoute);
 app.use("/api", listSchoolsRoute);
 
+
 // Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
